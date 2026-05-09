71699a84aed7411d8d0d1b369dc92d4a238288c5
kushal tamang <kushaltamang0926@gmail.com>
2026-05-09 11:49:39 +0545

    setting up the database  with neon and drizzle orm

---
+++ src/lib/database/schema.ts
@@ -0,0 +1,16 @@
+import {
+  pgTable,
+  integer,
+  timestamp,
+  varchar,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable("users", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  name: varchar({ length: 255 }).notNull(),
+  email: varchar({ length: 255 }).notNull().unique(),
+  emailVerified: boolean("email_verified").notNull().default(false),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+});
